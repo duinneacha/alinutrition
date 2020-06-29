2c6f75b2ce9bf9190305e9243641953c4ae4abed
Aidan Dennehy <duinneacha@gmail.com>
2020-06-29 23:18:55 +0100

    Large Screen Nav Fixed

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,30 @@
+
+const sideMenu  = document.getElementById('side-menu');
+const closeButton = document.getElementById('btn-close');
+console.log(closeButton);
+
+closeButton.addEventListener('click', (event) => {
+  console.log('Clicking Close Button');
+  sideMenu.style.width = '0';
+})
+
 function openSlideMenu(){
-  document.getElementById('side-menu').style.width = '250px';
-  document.getElementById('main').style.marginLeft = '250px';
+  console.log("Opening Side Menu");
+  // document.getElementById('side-menu').style.width = '250px';
+  sideMenu.style.width = '250px';
+  // document.getElementById('main').style.marginLeft = '250px';
 }
 
-function closeSlideMenu(){
-  document.getElementById('side-menu').style.width = '0';
+function closeSlideMenu(event){
+  event.preventDefault();
+  console.log(event);
+  sideMenu.style.width = '0';
   document.getElementById('main').style.marginLeft = '0';
 }
+
+// window.addEventListener('mouseup', function(event){
+  
+//   if (event.target != sideMenu && event.target.parentNode != sideMenu){
+//       sideMenu.style.display = 'none';
+//   }
+// });
